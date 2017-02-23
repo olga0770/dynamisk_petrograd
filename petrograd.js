aebbf661a948baf6e30d410f8ab4e48f73ca0b35
olga0770 <olyalo0201@gmail.com>
2017-02-23 16:42:54 +0100

    add data navn, pris and rabatpris

--- petrograd.js
+++ petrograd.js
@@ -5,10 +5,6 @@ console.log("siden vises");
 
 //læs produktliste
 $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
-
-
-
-visProdukt();
 }
 
 function visProduktListe( listen ){
@@ -23,12 +19,18 @@ function visProdukt( produkt ) {
     //klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 
+
     //indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = Math.ceil( produkt.pris - (produkt.pris*produkt.rabatsats/100) );
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
+
 
     //append klon til .produkt_liste
-
-document.querySelector(".produktliste").appendChild(klon);
-}
+    document.querySelector(".produktliste").appendChild(klon);
+    }
 
 
 
